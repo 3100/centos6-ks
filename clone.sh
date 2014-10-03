628d2c36dff0cdecce8bbf7a221e46c77da5a486
3100 <sugar16g@gmail.com>
2014-10-03 19:28:23 +0900

    modify to unmount cdrom

--- clone.sh
+++ clone.sh
@@ -3,9 +3,13 @@ PWD=`pwd`
 CLONE_DIR="${PWD}/img"
 SRC_DIR="${PWD}"
 
-if [ ! -d /media/ ]; then
-  sudo mount /dev/cdrom /media/
+DEV_DIR="/dev/cdrom"
+MNT_DIR="/media/"
+
+if [ ! -d ${MNT_DIR} ]; then
+  sudo mount ${DEV_DIR} ${MNT_DIR}
 fi
-( cd /media/; sudo find . -maxdepth 1 -mindepth 1 -exec cp -rp {} ${CLONE_DIR}/ \;)
+( cd ${MNT_DIR}; sudo find . -maxdepth 1 -mindepth 1 -exec cp -rp {} ${CLONE_DIR}/ \;)
+sudo umount ${DEV_DIR}
 sudo cp ${SRC_DIR}/isolinux.cfg ${CLONE_DIR}/isolinux -f
 exit 0
